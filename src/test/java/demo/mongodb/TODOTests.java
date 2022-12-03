cf382503c7c42c124251dcfe673191c4ba86313a
martineez <martineez@gmail.com>
2022-12-03 11:51:48 +0100

    updated deps

--- src/test/java/demo/mongodb/TODOTests.java
+++ src/test/java/demo/mongodb/TODOTests.java
@@ -10,8 +10,8 @@ import org.testng.annotations.Test;
 
 import java.net.UnknownHostException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 /**
  * Created with IntelliJ IDEA.
